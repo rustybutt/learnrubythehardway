7f9738c5b44690385059888e40e7bf06455016bf
Russ Button <russ@russbutton.com>
2017-04-24 13:11:34 -0700

    exercise 16

--- ex13.rb
+++ ex13.rb
@@ -3,12 +3,7 @@ puts "The script is called: #{$0}"
 puts "The first variable is: #{first}"
 puts "The second variable is: #{second}"
 puts "The third variable is: #{third}"
-
-while true do
-  inputvalue = gets.chomp
-  if inputvalue == "exit"
-    exit
-  end
-  puts inputvalue
-  
-end
+puts "\n\n******************\n\n"
+puts "The first variable is: #{$1}"
+puts "The second variable is: #{$2}"
+puts "The third variable is: #{$3}"
